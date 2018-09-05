013faa0ec6b8436ab07f2119a78258393bb2cdd5
Eric Mittelhammer <emittelhammer@newrelic.com>
2018-09-04 22:57:40 -0400

    fixed module.exports

--- build_events/tag-build.js
+++ build_events/tag-build.js
@@ -1,6 +1,6 @@
 const output =
 `
-module.export = {
+module.exports = {
     commit_sha: '${process.env['CODEBUILD_RESOLVED_SOURCE_VERSION']}',
     build_id: '${process.env['CODEBUILD_BUILD_ID']}'
 }
