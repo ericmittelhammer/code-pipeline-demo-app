ef751e8b28ed094398a8990c9f61bad23a27623d
Eric Mittelhammer <emittelhammer@newrelic.com>
2018-09-10 14:54:19 -0400

    use koa agent

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const Koa = require('koa');
 const Router = require('koa-router');
-const newrelic = require('newrelic');
+const newrelic = require('@newrelic/koa');
 
 const app = new Koa();
 const router = new Router();
